db0a03bed2ebb4359714463f165134c63c705821
suny-am <visualarea.1@gmail.com>
2024-03-28 11:01:53 +0100

    SuperConsole -> SharpConsole

--- TextColorException.cs
+++
@@ -1,6 +0,0 @@
-internal class TextColorException : Exception
-{
-    private readonly string message = "text colors have to be of Dictionary<string, ConsoleColor> type";
-
-    public override string Message => message;
-}
